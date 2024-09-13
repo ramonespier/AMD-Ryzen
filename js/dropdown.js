015143fd269627c832cbd1959939e12b268bcbce
ramonespier <ramon.coelhomelo@outlook.com>
2024-09-13 09:40:30 -0300

    Remake (nao responsiva) AMD Ryzen - in progress

---
+++ js/dropdown.js
@@ -0,0 +1,7 @@
+const dropdown = document.querySelector('.corpo-dropdown');
+
+document.querySelector('.btDropdown').addEventListener('click', () => {
+    dropdown.style.display = 'block';
+
+});
+
