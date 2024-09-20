ff38025cc2f4992fcb3a6c407679ad4e58290aca
ramonespier <ramon.coelhomelo@outlook.com>
2024-09-20 08:05:49 -0300

    feat: dropdown funcionando

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,41 @@
 const dropdown = document.querySelector('.corpo-dropdown');
+const botaoDrop = document.querySelector('#btDropdown');
 
-document.querySelector('.btDropdown').addEventListener('click', () => {
-    dropdown.style.display = 'block';
+botaoDrop.addEventListener('click', () => {
+    if (botaoDrop.classList.contains('active')) {
+        dropdown.style.animation = 'fecharAnimacao 0.5s forwards';
+        setTimeout(() => {
+            dropdown.style.display = 'none';
+            botaoDrop.classList.add('btDropdown');
+            botaoDrop.classList.remove('active');
+            dropdown.style.animation = '';
+        }, 500);
 
+
+    } else {
+        dropdown.style.display = 'block';
+        dropdown.style.display = 'animacao 0.5s forwards';
+        botaoDrop.classList.add('active')
+        botaoDrop.classList.remove('btDropdown')
+    }
 });
+
+document.addEventListener('click', (event) => {
+    if (botaoDrop.classList.contains('active') && !dropdown.contains(event.target) && event.target !== botaoDrop) {
+        dropdown.style.animation = 'fecharAnimacao 0.5s forwards';
+        setTimeout(() => {
+            dropdown.style.display = 'none';
+            botaoDrop.classList.add('btDropdown');
+            botaoDrop.classList.remove('active');
+            dropdown.style.animation = '';
+        }, 500)
+    }
+});
+
+
+
+
+
+
+
 
